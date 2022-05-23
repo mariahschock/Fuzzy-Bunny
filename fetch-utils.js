896e002f55500a622e09ed7cea7b42b69fb9f52d
Mariah <mariahschock@gmail.com>
2022-05-23 15:28:22 -0700

    delete function

--- fetch-utils.js
+++ fetch-utils.js
@@ -18,9 +18,13 @@ export async function getFamilies() {
     }
 }
 export async function deleteBunny(id) {
+    const response = await client.from('fuzzy_bunnies').delete().eq('id', id);
+    if (response.error) {
+        console.error(response.error.message);
+    } else {
+        return response.data;
+    }
     // delete a single bunny using the id argument
-
-    return checkError(response);
 }
 
 export async function createBunny(bunny) {
